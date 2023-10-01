4de59d6ea126e2b0128179d33267a0dd5596d3f3
Roni7823 <siy_sar78@yahoo.se>
2023-10-01 18:41:26 +0200

    Debugged

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
-//Breakpoint
+/*Breakpoint.
+Breakpoint på line 9.Man trycker bredvid 9 och det blir röd punkt, sedan trycker man på Debug knappen
+bredvid play knappen och kör programmet i debug mode. Längre ner bredvid RUN finns Debug mode där koden körs.
+Console visar Debugged kod. */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
